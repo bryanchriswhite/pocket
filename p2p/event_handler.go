faf41dad78d7b019ec5cc46f8471c86037691714
Bryan White <bryanchriswhite@gmail.com>
2023-04-24 12:36:30 +0200

    fixup: always bootstrap

--- p2p/event_handler.go
+++ p2p/event_handler.go
@@ -3,10 +3,11 @@ package p2p
 import (
 	"fmt"
 
+	"google.golang.org/protobuf/types/known/anypb"
+
 	"github.com/pokt-network/pocket/shared/codec"
 	coreTypes "github.com/pokt-network/pocket/shared/core/types"
 	"github.com/pokt-network/pocket/shared/messaging"
-	"google.golang.org/protobuf/types/known/anypb"
 )
 
 // CONSIDERATION(#576): making this part of some new `ConnManager`.
@@ -50,13 +51,12 @@ func (m *p2pModule) HandleEvent(event *anypb.Any) error {
 		m.logger.Debug().Fields(messaging.TransitionEventToMap(stateMachineTransitionEvent)).Msg("Received state machine transition event")
 
 		if stateMachineTransitionEvent.NewState == string(coreTypes.StateMachineState_P2P_Bootstrapping) {
-			if m.stakedActorRouter.GetPeerstore().Size() == 0 {
-				m.logger.Warn().Msg("No peers in addrbook, bootstrapping")
+			m.logger.Info().Msg("Bootstrapping...")
 
-				if err := m.bootstrap(); err != nil {
-					return err
-				}
+			if err := m.bootstrap(); err != nil {
+				return err
 			}
+
 			m.logger.Info().Bool("TODO", true).Msg("Advertise self to network")
 			if err := m.GetBus().GetStateMachineModule().SendEvent(coreTypes.StateMachineEvent_P2P_IsBootstrapped); err != nil {
 				return err
